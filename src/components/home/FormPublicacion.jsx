a5f6c7e3f43791cd4eefae37bbacc3f3737a03a4
edson.sarmiento <eaargueta@innovacion.gob.sv>
2025-07-07 14:10:00 -0600

    fix:ajustar tamaño de FormPublicacion

--- src/components/home/FormPublicacion.jsx
+++ src/components/home/FormPublicacion.jsx
@@ -5,7 +5,7 @@ import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 const FormPublicacion = () => {
     return (
         <div className="flex flex-col fixed bg-white items-center pt-18 p-4 w-full">
-            <div className="flex flex-col p-4 shadow-lg w-1/2 rounded-lg space-y-2">
+            <div className="flex flex-col p-4 shadow-lg w-4/6 rounded-lg space-y-2">
                 <input className="bg-white p-2 border rounded-md border-gray-400" type="text" placeholder="¿que estas pensando?"></input>
                 <div className="flex flex-row justify-between">
                     <div className="flex flex-row space-x-3 text-gray-600">
